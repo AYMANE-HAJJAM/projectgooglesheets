c4e30d064048ed28fb8eb8d9da0bf073a9699e1a
AYMANE-HAJJAM <aym.hajjam@gmail.com>
2025-09-06 23:41:23 +0100

    first commit

---
+++ assets/config.js
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  "https://script.google.com/macros/s/AKfycbwUVcQO7YXrqaoYeNrZ1AibiLQd44t5zwu9dRTUjtXcWLkz_tJV6Lpg21GEdIg2DWN0eA/exec";
+
+export const SECRET_KEY = "geeks-secret";
